6987d053325ff3c52c3208e2516d4bca01687949
Artur Bylak <Artur.Bylak@cybercom.com>
2015-05-08 17:39:14 +0200

    Added server

---
+++ src/main/java/com/bylak/vertx/server/Server.java
@@ -0,0 +1,39 @@
+package com.bylak.vertx.server;
+
+import org.vertx.java.core.Future;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.HttpServer;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.http.RouteMatcher;
+import org.vertx.java.platform.Verticle;
+
+/**
+ * Created by Artur.Bylak on 2015-05-08.
+ */
+public class Server extends Verticle {
+    @Override
+    public void start(final Future<Void> startedResult) {
+        HttpServer server = vertx.createHttpServer();
+        RouteMatcher routeMatcher = new RouteMatcher();
+        
+        routeMatcher.get("/page", new Handler<HttpServerRequest>() {
+            public void handle(HttpServerRequest request) {
+                System.out.println(
+                        "A request has arrived on the server!, thread " + Thread.currentThread()
+                                                                                .getId());
+                doSleep();
+                request.response().end();
+            }
+        });
+
+        server.requestHandler(routeMatcher).listen(8080, "localhost");
+    }
+
+    private void doSleep() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/bylak/vertx/server/ServerStarter.java
@@ -0,0 +1,30 @@
+package com.bylak.vertx.server;
+
+import java.util.function.Consumer;
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.AsyncResultHandler;
+import org.vertx.java.core.Future;
+import org.vertx.java.platform.Verticle;
+
+/**
+ * Created by Artur.Bylak on 2015-05-08.
+ */
+public class ServerStarter extends Verticle {
+    @Override
+    public void start(final Future<Void> startedResult) {
+        // For example - deploy some other verticle
+        System.out.println("Starter  thread : " + Thread.currentThread().getId());
+        deployVerticle("Server.java", 2, null);
+    }
+
+    private void deployVerticle(final String path, final int instances, final Consumer<Void>
+            consumer) {
+        container.deployVerticle(path, instances, new AsyncResultHandler<String>() {
+            public void handle(AsyncResult<String> deployResult) {
+                if (deployResult.succeeded() && consumer != null) {
+                    consumer.accept(null);
+                }
+            }
+        });
+    }
+}
